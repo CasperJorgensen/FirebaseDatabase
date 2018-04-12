bfdd010a082a247011036b4e0e98e92955adc2cf
Casper Jørgensen <casperj1805@gmail.com>
2018-04-12 09:47:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/caspe/firebasedatabase/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.caspe.firebasedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private int SIGN_IN_REQUEST_CODE = 1;
+    private String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference().child("message");
+
+
+        TextView textView = (TextView)findViewById(R.id.text);
+        textView.setText(myRef.toString());
+
+        myRef.setValue("Hello dickmonger");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null){
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        }
+        else {
+            Toast.makeText(this, "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName()
+                    , Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+}
